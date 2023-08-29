e395bf90a14cdba939141f80ed5f0fd907cd37e6
Smit Rajguru <rajgurusmit1997@gmail.com>
2023-08-29 17:25:57 -0400

    Add files via upload

--- script/audubon_unity.sh
+++ script/audubon_unity.sh
@@ -2,5 +2,7 @@
 
 echo "Audubon Unity Script"
 
-# download the latest release of the unity simulator
-wget 
+# download the latest audubon unity package
+git clone https://github.com/SmitRajguru/UB_CSE468_568_audubon_unity.git
+cp -R UB_CSE468_568_audubon_unity/* ./
+rm -rf UB_CSE468_568_audubon_unity
