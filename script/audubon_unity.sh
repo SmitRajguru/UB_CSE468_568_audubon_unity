92c7f8166cd29b0f997214d8331861f6f6b85384
Smit Rajguru <rajgurusmit1997@gmail.com>
2023-11-28 19:01:31 -0500

    pathing for sh

--- script/audubon_unity.sh
+++ script/audubon_unity.sh
@@ -3,6 +3,23 @@
 echo "Audubon Unity Script"
 
 # download the latest audubon unity package
+source /opt/ros/noetic/setup.bash
+
+# check if the catkin_ws folder exists
+if [ ! -d "/home/cse4568/catkin_ws" ]; then
+    echo "catkin_ws does not exist"
+    # check if the catkin_noetic folder exists
+    if [ ! -d "/mnt/c/catkin_noetic" ]; then
+        echo "catkin_ws does not exist"
+    fi
+    else
+        source /mnt/c/catkin_noetic/devel/setup.bash
+    fi
+fi
+else
+    source /home/cse4568/catkin_ws/devel/setup.bash
+fi
+
 roscd audubon_unity
 echo "Running in $(pwd)"
 git clone https://github.com/SmitRajguru/UB_CSE468_568_audubon_unity.git
@@ -10,3 +27,4 @@ cp -R UB_CSE468_568_audubon_unity/* ./
 rm -rf UB_CSE468_568_audubon_unity
 chmod +x script/*
 chmod +x src/*
+
